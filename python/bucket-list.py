ae85b7cb556232e192a655ed439b4869804e03d9
Austine <aanyangbeso@gmail.com>
2023-08-04 15:58:39 +0100

    change to the python code

--- python/bucket-list.py
+++ python/bucket-list.py
@@ -1,6 +1,7 @@
 import boto3
 
-s3 = boto3.client('s3')
+
+s3 = boto3.client('s3', region_name='us-east-1')
 buckets = s3.list_buckets()["Buckets"]
 for bucket in buckets:
     print(bucket['Name'])
